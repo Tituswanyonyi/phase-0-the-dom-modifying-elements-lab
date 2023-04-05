c9e81183204bd1aa3f5849b61c2f4b65121f38a5
titus <titus.unavailable@student.moringaschool.com>
2023-04-05 16:03:07 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new header element with an id of 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new header element
+const yourName = 'Titus Wanyonyi'; // Replace with your own name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Add the new header element to the document
+const body = document.querySelector('body');
+body.appendChild(newHeader);
